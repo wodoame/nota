e0ce720082074a049f51f5cdb736da126fe1161b
Wodoame <bmwodoame@gmail.com>
2026-02-17 09:36:09 +0000

    Sprint 2: chore: configure H2 database for testing profile

--- src/test/java/com/amalitech/nota/NotaApplicationTests.java
+++ src/test/java/com/amalitech/nota/NotaApplicationTests.java
@@ -3,7 +3,10 @@ package com.amalitech.nota;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class NotaApplicationTests {
 
 	@Test
