49c624378f4fc1a46f245a017d1a698e5d1dca6e
Wodoame <bmwodoame@gmail.com>
2026-02-17 07:52:27 +0000

    feat(testing): Implement unit tests for NoteController and update dependencies
    - Add NoteControllerTest to verify note operations
    - Update pom.xml for testing dependencies

--- src/test/java/com/amalitech/nota/controller/NoteControllerTest.java
+++ src/test/java/com/amalitech/nota/controller/NoteControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +23,7 @@ class NoteControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private NoteRepository noteRepository;
 
     @Test
